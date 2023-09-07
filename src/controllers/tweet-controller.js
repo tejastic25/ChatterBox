6f365dc0ca45ee6c16bb43f6fc0ebe73f9772099
Tejas <tejashsankhla@gmail.com>
2023-09-07 14:51:15 +0530

    added image upload option in tweet

--- src/controllers/tweet-controller.js
+++ src/controllers/tweet-controller.js
@@ -1,43 +1,52 @@
 import TweetService from "../service/tweet-service.js";
 import { StatusCodes } from "http-status-codes";
+import upload from "../config/file-upload-s3-config.js";
 const tweetService = new TweetService();
-
+const singleUploader = upload.single("image");
 const create = async (req, res) => {
-    try {
-        const tweet = await tweetService.create(req.body);
-        return res.status(StatusCodes.CREATED).json({
-            data: tweet,
-            success: true,
-            message: "successfully created tweet",
-            err: {}
-        });
+  try {
+    singleUploader(req, res, async function (err, data) {
+      console.log(err);
 
-    } catch (error) {
-        return res.status(StatusCodes.CREATED).json({
-            data: {},
-            success: false,
-            message: "not able to create tweet",
-            err: error
-        });
-    }
-}
+      if (err) {
+        return res.status(500).json({ error: err });
+      }
+      const payload = { ...req.body };
+      payload.image = req.file.location;
+      const response = await tweetService.create(payload);
+      return res.status(201).json({
+        success: true,
+        message: "Successfully created a new tweet",
+        data: response,
+        err: {},
+      });
+    });
+  } catch (error) {
+    return res.status(StatusCodes.CREATED).json({
+      data: {},
+      success: false,
+      message: "not able to create tweet",
+      err: error,
+    });
+  }
+};
+
 const getTweet = async (req, res) => {
-    try {
-        const tweet = await tweetService.getTweet(req.params.id);
-        return res.status(StatusCodes.OK).json({
-            data: tweet,
-            success: true,
-            message: "successfully fetched tweet",
-            err: {}
-        });
-
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            data: {},
-            success: false,
-            message: "not able to fetch tweet",
-            err: error
-        });
-    }
-}
-export { create, getTweet }
+  try {
+    const tweet = await tweetService.getTweet(req.params.id);
+    return res.status(StatusCodes.OK).json({
+      data: tweet,
+      success: true,
+      message: "successfully fetched tweet",
+      err: {},
+    });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      data: {},
+      success: false,
+      message: "not able to fetch tweet",
+      err: error,
+    });
+  }
+};
+export { create, getTweet };
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -17,6 +17,9 @@ const tweetSchema = new mongoose.Schema(
         ref: "Comment",
       },
     ],
+    image: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
